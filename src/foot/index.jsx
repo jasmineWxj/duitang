dc9d5d99dd86596a7324f8cf54b56069b526a133
wxjing46@163.com <wxjing46@163.com>
2022-08-13 22:18:09 +0800

    feat: 首页tab切换

--- src/foot/index.jsx
+++ src/foot/index.jsx
@@ -2,27 +2,32 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import classnames from 'classnames'
 import './index.scss'
+import '../common/css/iconfont.css'
 
 const Footer = () => {
     const { pathname } = useLocation()
     function path(params){
         console.log(params, pathname);
         if ('/' + params === pathname){
-            return 'actives_' + params
+            return 'current actives_' + params
         }
     }
     return (
         <div className='footer'>
             <Link to="/" className={path('')}>
+                <span class="iconfont icon-shouye"></span>
                 <div>首页</div>
             </Link>
             <Link to="/find" className={path('find')}>
+                <span class="iconfont icon-faxian"></span>
                 <div>发现</div>
             </Link>
             <Link to="/vip" className={path('vip')}>
+                <span class="iconfont icon-ziyuan"></span>
                 <div>专享</div>
             </Link>
             <Link to="/mine" className={path('mine')}>
+                <span class="iconfont icon-wo"></span>
                 <div>我</div>
             </Link>
         </div>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,18 @@
+
+import React from 'react'
+import '../style/home.scss'
+import { Tabs } from 'antd-mobile'
 function Home() {
     return (
         <div className="Home">
-           Home
+                <Tabs>
+                    <Tabs.Tab title='水果' key='fruits'>
+                    水果
+                    </Tabs.Tab>
+                    <Tabs.Tab title='蔬菜' key='vegetables'>
+                        西红柿
+                    </Tabs.Tab>
+                </Tabs>
         </div>
     );
 }
