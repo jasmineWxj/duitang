91f62bd781e58b6ab2daed291dd51279dcb24b69
wxj01295860 <wxjing46@163.com>
2022-08-15 15:48:09 +0800

    feat: test

--- src/component/Img.jsx
+++ src/component/Img.jsx
@@ -4,33 +4,34 @@ import { Image } from 'antd-mobile'
 import '../style/recommend.scss'
 import { debounce, getTop } from '../common/js/fangdou'
 const Img = () => {
-    const [imglist,setImglist] = useState([])
+    const [imglist,setImglist] = useState([]);
     const [start, setStart] = useState(0)
     useEffect(()=>{
         getImgList(start).then(res=>{
-            if (res.status === 1){
+            if (res.status === 1 && res.data.object_list.length > 1){
                 setStart(start+24)
-                setImglist(res.data.object_list)
-                console.log(imglist[0]);
+                console.log(res.data.object_list,'res.data.object_list');
+                setImglist([...res.data.object_list])
 
             }
         })
           
-        function scroll(){
-            var aa = document.querySelector('.more')
-            if (document.body.clientHeight + document.documentElement.scrollTop - 100 >= getTop(aa)) {
-                getImgList(start).then(res=>{
-                    if (res.status === 1) {
-                        setImglist(imglist.concat(res.data.object_list))
-                        setStart(start + 24)
-                    }
-                })
-            }
-        }
-        window.onscroll = debounce(scroll,200,true)
+        // function scroll(){
+        //     var aa = document.querySelector('.more')
+        //     if (document.body.clientHeight + document.documentElement.scrollTop - 100 >= getTop(aa)) {
+        //         getImgList(start).then(res=>{
+        //             if (res.status === 1) {
+        //                 // setImglist(imglist.concat(res.data.object_list))
+        //                 setStart(start + 24)
+        //             }
+        //         })
+        //     }
+        // }
+        // window.onscroll = debounce(scroll,200,true)
 
         
     }, [])
+    console.log(imglist,12);
     return (
         <div className='Img'>
             {
